f736e660f0e0709111a13f64fcdb452e59eec2d1
nosilv <nosilv8@gmail.com>
2023-04-12 19:52:35 +0900

    2주차 과제

--- assignment_univ/CH2_PROGRAMMING_QUIZ_1/ch2OpenChallenge.cpp
+++ assignment_univ/CH2_PROGRAMMING_QUIZ_1/ch2OpenChallenge.cpp
@@ -13,7 +13,7 @@ int main(void) {
 	cin >> t;
 
 	// 2. 누가 이기고 졌는지 판단하고 출력한다.
-	if (s == t)
+	if (!strcmp(s, t))
 		cout << "비겼습니다" << endl;
 	else if ((s == "가위" && t == "보") || (s == "바위" && t == "가위") || (s == "보" && t == "바위"))
 		cout <<"로미오가 이겼습니다." << endl;
@@ -22,3 +22,49 @@ int main(void) {
 
 	return 0;
 }
+
+/* Solution
+// ch2OpenChallenge
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "가위 바위 보 게임을 합니다. 가위, 바위, 보 중에서 입력하세요." << endl;
+
+	// 입력 받기
+	char s[10];
+	cout << "로미오>>";
+	cin >> s;
+
+	char t[10];
+	cout << "줄리엣>>";
+	cin >> t;
+
+	// 틀린 입력 골라내기
+	if(strcmp(s, "가위") != 0 && strcmp(s, "바위") != 0 && strcmp(s, "보") != 0) {
+		cout << "로미오의 입력이 틀렸습니다." << endl;
+		return 0;
+	}
+	if(strcmp(t, "가위") != 0 && strcmp(t, "바위") != 0 && strcmp(t, "보") != 0) {
+		cout << "줄리엣의 입력이 틀렸습니다." << endl;
+			return 0;
+	}
+
+	// 승자 결정
+	if(strcmp(s, "가위") == 0 && strcmp(t, "보") == 0)
+		cout << "로미오가 이겼습니다." << endl;
+	else if(strcmp(s, "가위") == 0 && strcmp(t, "바위") == 0)
+		cout << "줄리엣이 이겼습니다." << endl;
+	else if(strcmp(s, "바위") == 0 && strcmp(t, "가위") == 0)
+		cout << "로미오가 이겼습니다." << endl;
+	else if(strcmp(s, "바위") == 0 && strcmp(t, "보") == 0)
+		cout << "줄리엣이 이겼습니다." << endl;
+	else if(strcmp(s, "보") == 0 && strcmp(t, "바위") == 0)
+		cout << "로미오가 이겼습니다." << endl;
+	else if(strcmp(s, "보") == 0 && strcmp(t, "가위") == 0)
+		cout << "줄리엣이 이겼습니다." << endl;
+	else
+		cout << "비겼습니다." << endl;
+}
+*/
